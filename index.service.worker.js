f3789e4595848cfa615a2f21f276b668e40c6e5b
Jared VandeWater <74475048+JaredVandeWater@users.noreply.github.com>
2024-03-20 22:40:17 -0600

    updated with addon

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1710992934|401896219";
+const CACHE_VERSION = "1710995947|3414502412";
 const CACHE_PREFIX = "RealmTracker-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
